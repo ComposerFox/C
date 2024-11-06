b9306f6930431e8f3a42957b384482a7f163c627
ComposerFox <leesunghyun2580916@gmail.com>
2024-11-06 21:51:23 +0900

    2024-11-06_1

--- Loop_prj/Loop_prj/loopEx.c
+++ Loop_prj/Loop_prj/loopEx.c
@@ -80,5 +80,69 @@ int main(void)
 	}
 	printf("%d ~ %d의 합(3의 배수 제외) = %d\n", start, y, sum);
 
+	dan = 0;
+
+	for (int i = 1; i <= 9; i++)
+	{
+		printf("%d * %d = %d \n", dan, i, dan * i);
+	}
+
+	start = 1;
+	end = 10;
+	sum = 0;
+
+	for (int i = start; i <= end; i += 2)
+	{
+		sum += i;
+	}
+	printf("%d ~ %d의 홀수합 = %2d \n", start, end, sum);
+
+	sum = 0;
+	inData = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d번째 정수 입력", i + 1);
+		scanf("%d", &inData);
+		sum += inData;
+	}
+
+	printf("입력한 정수의 합 = %2d \n", sum);
+
+	j = 0;
+
+	for (int i = 0; i <= 5; i++)
+	{
+		for (j; j < i; j++)
+		{
+			printf(" * ");
+		}
+		printf("\n");
+	}
+
+	for (dan = 2; dan <= 9; dan++)
+	{
+		printf("----- %d단 -----\n");
+
+		for (int i = 0; i <= 9; i++)
+		{
+			printf("%d * %d = %d \n", dan, i, dan * i);
+		}
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (i % 4 == 0)
+			continue;
+		printf(" %d ", i);
+	}
+
+	int i = 1000;
+
+	do {
+		printf("i = %d\n");
+		i++;
+	} while (i < 3);
+
 	return 0;
 }
